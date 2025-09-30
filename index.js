6d4cc7a4f824e70e185571acfe7732fe119aa531
Toufiq-hash <mentomanta6@gmail.com>
2025-09-30 11:22:33 +0600

    6th

--- index.js
+++ index.js
@@ -1329,8 +1329,8 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server error", error: err.message });
 });
 
-
 const port = process.env.PORT || 3000;
 app.listen(port);
+
 
 
